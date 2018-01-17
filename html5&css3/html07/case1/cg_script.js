968a87b1ac75171d3143509935b8ca8481092f2c
Cui Xinzhou <toxicstrikercuixinzhou@gmail.com>
2018-01-17 08:03:43 -0500

    Add files via upload

---
+++ html5&css3/html07/case1/cg_script.js
@@ -0,0 +1,33 @@
+/*
+   New Perspectives on HTML and CSS, 7th Edition
+   Tutorial 7
+   Case Problem 1
+
+
+   Filename: cg_script.js
+
+   Purpose: The purpose of this program is to verify that the form
+            passes an initial validation test.
+
+            When the form is submitted, the onsubmit event handler
+            verifies that the form data is complete and valid.
+            An alert box is displayed notifying the user.
+
+            The event function returns a value of false so that the
+            student does not have to continually retype test values
+            in the survey form.
+
+
+*/
+
+window.onload = init;
+
+function init() {
+   document.forms[0].onsubmit = function() {
+      if (this.checkValidity()) alert("Data passes initial validation tests");
+      return false;
+   }
+   
+}
+
+  
